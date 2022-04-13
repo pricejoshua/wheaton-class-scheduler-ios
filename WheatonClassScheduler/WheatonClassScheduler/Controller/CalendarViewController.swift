62b974940d69972bce37cdc77bf41bcaadc3d16b
Joshua Price <joshua.j.price@my.wheaton.edu>
2022-04-13 17:16:31 -0500

    worked

---
+++ WheatonClassScheduler/WheatonClassScheduler/Controller/CalendarViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarViewController.swift
+//  WheatonClassScheduler
+//
+//  Created by Joshua Price on 4/13/22.
+//
+
+import UIKit
+
+class CalendarViewController: DayViewController {
+
+}
